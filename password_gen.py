e8a33e919722c6dc99d90131efd750ad1688d440
eakomdo <akomdoemmanuel22@gmail.com>
2025-07-08 23:25:55 +0000

    made a few changes

--- password_gen.py
+++ password_gen.py
@@ -2,7 +2,7 @@
 
 import random
 import string
-
+print("Hello! You are welcome to the password generator. Password length should be at least 8 charact.")
 
 def generate_password(length=12):
     characters = string.ascii_letters + string.digits + string.punctuation
@@ -12,7 +12,12 @@ def generate_password(length=12):
 def main():
     length = int(input("Enter the desired password length: "))
     include_special_characters = input("Include special characters? (yes/no): ").strip().lower() == 'yes'
-    
+    while True:
+        if length < 8:
+            print("Password length should be at least 8 characters. Please try again.")
+            length = int(input("Enter the desired password length: "))
+        else:
+            break
     if include_special_characters:
         characters = string.ascii_letters + string.digits + string.punctuation
     else:
