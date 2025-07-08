cdd73635149b39fbef75827e3a202ff8801fb8d2
eakomdo <akomdoemmanuel22@gmail.com>
2025-07-08 23:01:55 +0000

    updated my code

--- password_gen.py
+++ password_gen.py
@@ -6,4 +6,5 @@ import string
 
 def generate_password(length=12):
     characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
    
