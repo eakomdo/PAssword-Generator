8941ee434eef44df483378925dfa82a4a2a41520
eakomdo <akomdoemmanuel22@gmail.com>
2025-07-09 23:33:01 +0000

    made a few changes

--- password_gen.py
+++ password_gen.py
@@ -71,3 +71,4 @@ if __name__ == "__main__":
 # The password generator can be easily integrated into larger applications or used as a standalone script.
 # The code is compatible with Python 3 and can be run in any Python environment.
 # The password generator can be customized further by adding more options for character types or length constraints.
+# The code is designed to be robust and handle edge cases gracefully.
