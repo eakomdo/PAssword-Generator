994cbb920ccdccc5883393f171df7aa17f4f9f98
eakomdo <akomdoemmanuel22@gmail.com>
2025-07-14 23:55:32 +0000

    reworked on the code

--- passd.py
+++ passd.py
@@ -4,6 +4,7 @@
 # It is useful for scenarios where sensitive information needs to be entered without being visible on the screen.
 # The code is designed to be user-friendly and provides clear instructions for input.
 # The password is stored securely in memory and not displayed on the console.
+# The code is compatible with Python 3 and can be run in any Python environment.
 from getpass import getpass
 
 def main():
